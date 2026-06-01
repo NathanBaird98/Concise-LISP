219b363956730ae68dec5219bcfad47f408d2cc4
Nathan <241355844+NathanBaird98@users.noreply.github.com>
2026-06-01 14:03:30 +0100

    Re-factoring to the lval type enum and lval struct to store S-expressions as lval.

--- src/main.c
+++ src/main.c
@@ -29,14 +29,19 @@ void add_history(char *unused) {}
 #endif
 
 // Lisp Value Struct
-typedef struct {
+typedef struct lval {
 	int type;
 	long num;
-	int err;
+	// Error and Symbol types have some string data
+	char *err;
+	char *sym;
+	// Count and Pointer to a list of "lval pointer"
+	int count;
+	struct lval** cell;
 } lval;
 
 // Enumeration of possible lval Types
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 
 // Enumeration of possible Error Types
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
