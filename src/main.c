3dfef10f0695b880879473c1571f7a9fd807d85d
Nathan <241355844+NathanBaird98@users.noreply.github.com>
2026-06-01 14:57:19 +0100

    Functions to read expressions and construct and lval.

--- src/main.c
+++ src/main.c
@@ -210,6 +210,49 @@ lval eval(mpc_ast_t *t) {
 	return x;
 }
 
+/*
+ *
+ */
+lval * lval_read_num(mpc_ast_t *t) {
+	errno = 0;
+	long x = strtol(t->contents, NULL, 10);
+	return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
+}
+
+/*
+ * 
+ */
+lval *lval_read(mpc_ast_t *t) {
+	//If Symbol or Number return conversion to the type
+	if (strstr(t->tag, "number")) { return lval_read_num(t); }
+	if (strstr(t->tag. "symbol")) { return lval_sym(t->contents); }
+
+	// If root (>) or sexpr then create empty list
+	lval *x = NULL;
+	if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+	if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+	// Fill this list with any valid expression contained within
+	for (int i = 0; i < t->children_nu,; i++) {
+		if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+		if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+		if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+		x = lval_add(x, lval_read(t->children[i])};
+	}
+	
+	return x;
+}
+
+/*
+ *
+ */
+lval *lval_add(lval *v, lval *x) {
+	v->count++;
+	v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+	v->cell[v->count-1] = x;
+	return v;
+}
+
 int main(int argc, char **argv) {
 	// Create some parsers
 	mpc_parser_t *Number	= mpc_new("number");
